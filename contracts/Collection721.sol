deaab71fab1309d748e8fdd193a80199e1385228
Kisthanny <tigerqige@gmail.com>
2024-06-27 17:33:13 +0800

    Fix: test and debug for Collection721

--- contracts/Collection721.sol
+++ contracts/Collection721.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -12,6 +11,7 @@ contract Collection721 is ERC721, ERC721URIStorage, Ownable {
     using Strings for uint256;
 
     uint256 private _nextTokenId;
+    uint16 private _nextDropId;
     string public baseURI;
     string public logoURI;
     bool private _baseURISet;
@@ -22,13 +22,27 @@ contract Collection721 is ERC721, ERC721URIStorage, Ownable {
         uint256 mintLimitPerWallet;
         uint256 startTime;
         uint256 endTime;
+        uint256 price;
         bool hasWhiteListPhase;
         uint256 whiteListEndTime;
+        uint256 whiteListPrice;
         mapping(address => bool) whiteListAddresses;
         mapping(address => uint256) mintedPerWallet;
     }
 
-    Drop public currentDrop;
+    struct DropReturn {
+        uint256 supply;
+        uint256 minted;
+        uint256 mintLimitPerWallet;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 price;
+        bool hasWhiteListPhase;
+        uint256 whiteListEndTime;
+        uint256 whiteListPrice;
+    }
+
+    mapping(uint16 => Drop) private DropList;
 
     constructor(
         string memory _name,
@@ -39,32 +53,32 @@ contract Collection721 is ERC721, ERC721URIStorage, Ownable {
     }
 
     modifier onlyDuringDrop() {
+        Drop storage drop = DropList[_nextDropId];
         require(
-            block.timestamp >= currentDrop.startTime &&
-                block.timestamp <= currentDrop.endTime,
+            block.timestamp >= drop.startTime &&
+                block.timestamp <= drop.endTime,
             "Drop not active"
         );
         _;
     }
 
     modifier onlyAfterDrop() {
+        Drop storage drop = DropList[_nextDropId];
         require(
-            currentDrop.endTime != 0 && block.timestamp >= currentDrop.endTime,
+            drop.endTime != 0 && block.timestamp > drop.endTime,
             "Drop not end or no initial Drop"
         );
         _;
     }
 
     modifier canMint(uint256 amount) {
+        Drop storage drop = DropList[_nextDropId];
         require(
-            currentDrop.mintedPerWallet[msg.sender] + amount <=
-                currentDrop.mintLimitPerWallet,
+            drop.mintedPerWallet[msg.sender] + amount <=
+                drop.mintLimitPerWallet,
             "Mint limit per wallet exceeded"
         );
-        require(
-            currentDrop.minted + amount <= currentDrop.supply,
-            "Exceeds drop supply"
-        );
+        require(drop.minted + amount <= drop.supply, "Exceeds drop supply");
         _;
     }
 
@@ -73,14 +87,36 @@ contract Collection721 is ERC721, ERC721URIStorage, Ownable {
         _;
     }
 
+    function currentDrop() public view returns (DropReturn memory) {
+        Drop storage drop = DropList[_nextDropId];
+        DropReturn memory dropReturn = DropReturn(
+            drop.supply,
+            drop.minted,
+            drop.mintLimitPerWallet,
+            drop.startTime,
+            drop.endTime,
+            drop.price,
+            drop.hasWhiteListPhase,
+            drop.whiteListEndTime,
+            drop.whiteListPrice
+        );
+        return dropReturn;
+    }
+
+    function getBlockTime() public view returns (uint256) {
+        return block.timestamp;
+    }
+
     function getWhiteListAccess(
         address _userAddress
     ) public view returns (bool) {
-        return currentDrop.whiteListAddresses[_userAddress];
+        Drop storage drop = DropList[_nextDropId];
+        return drop.whiteListAddresses[_userAddress];
     }
 
     function getMintCount(address _userAddress) public view returns (uint256) {
-        return currentDrop.mintedPerWallet[_userAddress];
+        Drop storage drop = DropList[_nextDropId];
+        return drop.mintedPerWallet[_userAddress];
     }
 
     function setBaseURI(
@@ -94,26 +130,33 @@ contract Collection721 is ERC721, ERC721URIStorage, Ownable {
         logoURI = _logoURI;
     }
 
-    // for user
-    // they mint during drop
-    // tokenURI will be set by owner after drop ends
-    function safeMint(uint256 amount) public onlyDuringDrop canMint(amount) {
-        // check whiteList access during whiteList phase
+    function safeMint(
+        uint256 amount
+    ) public payable onlyDuringDrop canMint(amount) {
+        Drop storage drop = DropList[_nextDropId];
+        uint256 pricePerToken = drop.price;
         if (
-            currentDrop.hasWhiteListPhase &&
-            block.timestamp <= currentDrop.whiteListEndTime
+            drop.hasWhiteListPhase && block.timestamp <= drop.whiteListEndTime
         ) {
             require(
-                currentDrop.whiteListAddresses[msg.sender],
+                drop.whiteListAddresses[msg.sender],
                 "Only whitelist address can mint during whitelist phase."
             );
+            pricePerToken = drop.whiteListPrice;
         }
+        require(
+            pricePerToken * amount == msg.value,
+            "Please set the right value for minting"
+        );
+
+        payable(owner()).transfer(msg.value);
+
         for (uint256 i = 0; i < amount; i++) {
             uint256 tokenId = _nextTokenId++;
             _safeMint(msg.sender, tokenId);
         }
-        currentDrop.minted += amount;
-        currentDrop.mintedPerWallet[msg.sender] += amount;
+        drop.minted += amount;
+        drop.mintedPerWallet[msg.sender] += amount;
     }
 
     function createDrop(
@@ -121,27 +164,34 @@ contract Collection721 is ERC721, ERC721URIStorage, Ownable {
         uint256 _mintLimitPerWallet,
         uint256 _startTime,
         uint256 _endTime,
+        uint256 _price,
         bool _hasWhiteListPhase,
         uint256 _whiteListEndTime,
+        uint256 _whiteListPrice,
         address[] memory _whiteListAddresses
     ) external onlyOwner beforeUpload {
-        require(block.timestamp > currentDrop.endTime, "Ongoing drop exists");
+        Drop storage drop = DropList[_nextDropId];
+        require(block.timestamp > drop.endTime, "Ongoing drop exists");
 
-        currentDrop.supply = _supply;
-        currentDrop.minted = 0;
-        currentDrop.mintLimitPerWallet = _mintLimitPerWallet;
-        currentDrop.startTime = _startTime;
-        currentDrop.endTime = _endTime;
-        currentDrop.hasWhiteListPhase = _hasWhiteListPhase;
-        currentDrop.whiteListEndTime = _whiteListEndTime;
+        _nextDropId++;
 
-        // 设置白名单地址
+        Drop storage newDrop = DropList[_nextDropId];
+
+        newDrop.supply = _supply;
+        newDrop.minted = 0;
+        newDrop.mintLimitPerWallet = _mintLimitPerWallet;
+        newDrop.startTime = _startTime;
+        newDrop.endTime = _endTime;
+        newDrop.price = _price;
+        newDrop.hasWhiteListPhase = _hasWhiteListPhase;
+        newDrop.whiteListEndTime = _whiteListEndTime;
+        newDrop.whiteListPrice = _whiteListPrice;
+
         for (uint256 i = 0; i < _whiteListAddresses.length; i++) {
-            currentDrop.whiteListAddresses[_whiteListAddresses[i]] = true;
+            newDrop.whiteListAddresses[_whiteListAddresses[i]] = true;
         }
     }
 
-    // 重写tokenURI方法
     function tokenURI(
         uint256 tokenId
     ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
--- test/Collection721.ts
+++ test/Collection721.ts
@@ -1,11 +1,15 @@
-import { expect, assert } from "chai";
+import { expect } from "chai";
 import hre from "hardhat";
 import { Collection721 } from "../typechain-types";
-import { ContractTransactionResponse } from "ethers";
+import { ContractTransactionResponse, ethers } from "ethers";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
-const dateToTimestamp = (date: Date) => {
-    return date.getTime();
+const sleep = (timeout: number) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(null);
+        }, timeout)
+    })
 }
 
 describe("Drop", () => {
@@ -66,13 +70,33 @@ describe("Drop", () => {
             })
         })
 
-        const SUPPLY = 200;
+        const SUPPLY = 9;
         const MINT_LIMIT_PER_WALLET = 5;
-        const START_TIME = dateToTimestamp(new Date());
-        const END_TIME = dateToTimestamp(new Date("Wed Jun 26 2024 20:00:00 GMT+0800"));
+        let START_TIME: bigint;
+        let END_TIME: bigint;
+        const PRICE = ethers.parseEther("0.1");
         const HAS_WHITE_LIST_PHASE = true;
-        const WHITE_LIST_END_TIME = dateToTimestamp(new Date("Wed Jun 26 2024 19:00:00 GMT+0800"));
+        let WHITE_LIST_END_TIME: bigint;
+        const WHITE_LIST_PRICE = ethers.parseEther("0.01");
+        const BASE_URI = "ipfs://yyy"
         describe("During Drop", () => {
+            beforeEach(async () => {
+                START_TIME = await collection721.getBlockTime()
+                END_TIME = START_TIME + 4n;
+                WHITE_LIST_END_TIME = START_TIME + 2n;
+                const tx = await collection721.connect(owner).createDrop(
+                    SUPPLY,
+                    MINT_LIMIT_PER_WALLET,
+                    START_TIME,
+                    END_TIME,
+                    PRICE,
+                    HAS_WHITE_LIST_PHASE,
+                    WHITE_LIST_END_TIME,
+                    WHITE_LIST_PRICE,
+                    [user_1.address, user_2.address]
+                )
+                await tx.wait();
+            })
             it("Reject Drop by third-party", async () => {
                 try {
                     const tx = await collection721.connect(user_1).createDrop(
@@ -80,8 +104,10 @@ describe("Drop", () => {
                         MINT_LIMIT_PER_WALLET,
                         START_TIME,
                         END_TIME,
+                        PRICE,
                         HAS_WHITE_LIST_PHASE,
                         WHITE_LIST_END_TIME,
+                        WHITE_LIST_PRICE,
                         [user_1.address, user_2.address]
                     )
                     await tx.wait();
@@ -90,32 +116,172 @@ describe("Drop", () => {
                     expect((error as Error).message).include("revert")
                 }
             })
-            beforeEach(async () => {
-                const tx = await collection721.connect(owner).createDrop(
-                    SUPPLY,
-                    MINT_LIMIT_PER_WALLET,
-                    START_TIME,
-                    END_TIME,
-                    HAS_WHITE_LIST_PHASE,
-                    WHITE_LIST_END_TIME,
-                    [user_1.address, user_2.address]
-                )
-                await tx.wait();
-            })
             it("Sets Drop info", async () => {
                 const currentDrop = await collection721.currentDrop();
-                const { supply, mintLimitPerWallet, startTime, endTime, hasWhiteListPhase, whiteListEndTime } = currentDrop
+                const { supply, mintLimitPerWallet, startTime, endTime, price, hasWhiteListPhase, whiteListEndTime, whiteListPrice } = currentDrop
                 expect(supply).equal(SUPPLY);
                 expect(mintLimitPerWallet).equal(MINT_LIMIT_PER_WALLET);
                 expect(startTime).equal(START_TIME);
                 expect(endTime).equal(END_TIME);
+                expect(price).equal(PRICE);
                 expect(hasWhiteListPhase).equal(HAS_WHITE_LIST_PHASE);
                 expect(whiteListEndTime).equal(WHITE_LIST_END_TIME);
+                expect(whiteListPrice).equal(WHITE_LIST_PRICE);
                 const user_1_is_white = await collection721.connect(user_1).getWhiteListAccess(user_1.address);
                 const user_2_is_white = await collection721.connect(user_2).getWhiteListAccess(user_2.address);
                 const user_3_is_white = await collection721.connect(user_3).getWhiteListAccess(user_3.address);
                 expect([user_1_is_white, user_2_is_white].every(e => e)).true;
                 expect(user_3_is_white).false;
+            })
+
+            describe("During White List Phase", () => {
+                let ownerStartBalance: bigint;
+                beforeEach(async () => {
+                    ownerStartBalance = await hre.ethers.provider.getBalance(owner.address);
+                    const tx = await collection721.connect(user_1).safeMint(5, { value: WHITE_LIST_PRICE * 5n });
+                    await tx.wait();
+                })
+
+                it("Updates Minted", async () => {
+                    const currentDrop = await collection721.currentDrop()
+                    expect(currentDrop.minted).equal(5);
+                    const userMinted = await collection721.getMintCount(user_1.address)
+                    expect(userMinted).equal(5);
+                })
+
+                it("Confirms Ownership", async () => {
+                    const promiseList = [
+                        collection721.ownerOf(0),
+                        collection721.ownerOf(1),
+                        collection721.ownerOf(2),
+                        collection721.ownerOf(3),
+                        collection721.ownerOf(4),
+                    ]
+                    const res = await Promise.all(promiseList)
+                    expect(res.every(address => address === user_1.address)).true
+                })
+
+                it("Owner Receives Funds", async () => {
+                    const ownerCurrentBalance = await hre.ethers.provider.getBalance(owner.address);
+                    expect(ownerCurrentBalance - ownerStartBalance).equal(WHITE_LIST_PRICE * 5n)
+                })
+
+                it("Reject insufficient Funds", async () => {
+                    try {
+                        const tx = await collection721.connect(user_2).safeMint(2, { value: WHITE_LIST_PRICE });
+                        await tx.wait();
+                        throw new Error("mint should not proceed with insufficient funds");
+                    } catch (error) {
+                        expect((error as Error).message).include("revert")
+                    }
+                })
+
+                it("Reject Non-white-List mint", async () => {
+                    try {
+                        const tx = await collection721.connect(user_3).safeMint(5);
+                        await tx.wait();
+                        throw new Error("non white list user should not be able to mint during white list phase")
+                    } catch (error) {
+                        expect((error as Error).message).include("revert")
+                    }
+                })
+            })
+
+            describe("During Public Phase", () => {
+                beforeEach(async () => {
+                    await sleep(2000);
+                    const tx = await collection721.connect(user_3).safeMint(5, { value: PRICE * 5n });
+                    await tx.wait();
+                })
+                it("Updates Minted", async () => {
+                    const currentDrop = await collection721.currentDrop()
+                    expect(currentDrop.minted).equal(5);
+                    const userMinted = await collection721.getMintCount(user_3.address)
+                    expect(userMinted).equal(5);
+                })
+
+                it("Reject Mint Exceed Wallet Limit", async () => {
+                    try {
+                        const tx = await collection721.connect(user_3).safeMint(1, { value: PRICE });
+                        await tx.wait();
+                        throw new Error("should not exceed mintLimitPerWallet")
+                    } catch (error) {
+                        expect((error as Error).message).include("revert")
+                    }
+                })
+
+                it("Reject Mint Exceed Supply", async () => {
+                    try {
+                        const tx = await collection721.connect(user_1).safeMint(5, { value: PRICE * 5n });
+                        await tx.wait();
+                        throw new Error("should not exceed supply")
+                    } catch (error) {
+                        expect((error as Error).message).include("revert")
+                    }
+                })
+
+                it("Reject Upload", async () => {
+                    try {
+                        const tx = await collection721.connect(owner).setBaseURI(BASE_URI);
+                        await tx.wait()
+                        throw new Error("should not be able to upload")
+                    } catch (error) {
+                        expect((error as Error).message).include("revert")
+                    }
+                })
+
+                it("Reject Overlap Drop", async () => {
+                    try {
+                        const tx = await collection721.connect(owner).createDrop(SUPPLY,
+                            MINT_LIMIT_PER_WALLET,
+                            START_TIME,
+                            END_TIME,
+                            PRICE,
+                            HAS_WHITE_LIST_PHASE,
+                            WHITE_LIST_END_TIME,
+                            WHITE_LIST_PRICE,
+                            [user_1.address, user_2.address]);
+                        await tx.wait()
+                        throw new Error("should not be able to create new drop during ongoing drop")
+                    } catch (error) {
+                        expect((error as Error).message).include("revert")
+                    }
+                })
+            })
+
+            describe("After Drop", () => {
+                beforeEach(async () => {
+                    const tx_1 = await collection721.connect(user_1).safeMint(3, { value: WHITE_LIST_PRICE * 3n });
+                    await tx_1.wait();
+                    await sleep(2000);
+                    const tx_2 = await collection721.connect(user_3).safeMint(5, { value: PRICE * 5n });
+                    await tx_2.wait();
+                    await sleep(3000);
+                })
+
+                it("Could Create a New Drop", async () => {
+                    const tx = await collection721.connect(owner).createDrop(
+                        SUPPLY,
+                        MINT_LIMIT_PER_WALLET,
+                        START_TIME,
+                        END_TIME,
+                        PRICE,
+                        HAS_WHITE_LIST_PHASE,
+                        WHITE_LIST_END_TIME,
+                        WHITE_LIST_PRICE,
+                        [user_1.address]
+                    )
+                    await tx.wait();
+                    const access = await collection721.getWhiteListAccess(user_2.address)
+                    expect(access).false;
+                })
+
+                it("Sets Base URI", async () => {
+                    const tx = await collection721.connect(owner).setBaseURI(BASE_URI);
+                    await tx.wait()
+                    const firstURI = await collection721.tokenURI(0)
+                    expect(firstURI).equal(`${BASE_URI}/metadata/0`)
+                })
             })
         })
 
