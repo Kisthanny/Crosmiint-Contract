e870949e9b5665dbc27639e8882bf32d7a4bbc52
Kisthanny <tigerqige@gmail.com>
2024-07-02 14:02:21 +0800

    Add: crosschain transfer logic

--- contracts/Collection1155.sol
+++ contracts/Collection1155.sol
@@ -5,25 +5,98 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "./IGmpReceiver.sol";
+import "./IGateway.sol";
 
-contract Collection1155 is ERC1155, Ownable {
+contract Collection1155 is ERC1155, Ownable, IGmpReceiver {
     using Strings for uint256;
 
+    address public immutable _gateway;
     string public name;
     string public symbol;
     string public logoURI;
     uint256 private _nextTokenId;
+
+    struct CrosschainMessage {
+        uint256 tokenId;
+        string tokenURI;
+        address newHolder;
+        uint256 amount;
+    }
+
     mapping(uint256 => string) private _tokenURIs;
     mapping(uint256 => uint256) private _totalSupply;
+    mapping(uint16 => address) private _crosschainContract;
 
     constructor(
         string memory _name,
         string memory _symbol,
-        string memory _logoURI
+        string memory _logoURI,
+        address gateway
     ) ERC1155("") Ownable(msg.sender) {
         name = _name;
         symbol = _symbol;
         logoURI = _logoURI;
+        _gateway = gateway;
+    }
+
+    // asuming the gateway already handles verification
+    // we just need to mint on received
+    function onGmpReceived(
+        bytes32,
+        uint128,
+        bytes32,
+        bytes calldata b
+    ) external payable returns (bytes32) {
+        require(msg.sender == _gateway, "unauthorized");
+        CrosschainMessage memory message = abi.decode(b, (CrosschainMessage));
+        mintByGateway(
+            message.tokenId,
+            message.newHolder,
+            message.amount,
+            message.tokenURI
+        );
+        return bytes32("");
+    }
+
+    function crosschainTransfer(
+        uint256 _tokenId,
+        string calldata _tokenURI,
+        address _newHolder,
+        uint256 _amount,
+        uint16 _destinationNetwork
+    ) public {
+        require(
+            _crosschainContract[_destinationNetwork] != address(0),
+            "Crosschain: Unsupport Network"
+        );
+        require(
+            balanceOf(msg.sender, _tokenId) >= _amount,
+            "Crosschain: Insufficient amount owned by Sender"
+        );
+        burn(msg.sender, _tokenId, _amount);
+        IGateway gateway = IGateway(_gateway);
+        gateway.submitMessage(
+            _crosschainContract[_destinationNetwork],
+            _destinationNetwork,
+            3000000,
+            encodeCrosschainMessage(_tokenId, _tokenURI, _newHolder, _amount)
+        );
+    }
+
+    function encodeCrosschainMessage(
+        uint256 _tokenId,
+        string memory _tokenURI,
+        address _newHolder,
+        uint256 _amount
+    ) private pure returns (bytes memory) {
+        CrosschainMessage memory message = CrosschainMessage(
+            _tokenId,
+            _tokenURI,
+            _newHolder,
+            _amount
+        );
+        return abi.encode(message);
     }
 
     function uri(uint256 tokenId) public view override returns (string memory) {
@@ -39,6 +112,18 @@ contract Collection1155 is ERC1155, Ownable {
         _setTokenURI(_nextTokenId, metadataURI);
         _totalSupply[_nextTokenId] += amount;
         _nextTokenId++;
+    }
+
+    function mintByGateway(
+        uint256 tokenId,
+        address holder,
+        uint256 amount,
+        string memory metadataURI
+    ) private {
+        require(msg.sender == _gateway, "unauthorized");
+        _mint(holder, tokenId, amount, "");
+        _setTokenURI(tokenId, metadataURI);
+        _totalSupply[tokenId] += amount;
     }
 
     function mintBatch(
@@ -62,12 +147,11 @@ contract Collection1155 is ERC1155, Ownable {
         _mintBatch(msg.sender, ids, amounts, data);
     }
 
-    function burn(
-        address account,
-        uint256 id,
-        uint256 amount
-    ) public onlyOwner {
-        require(account == msg.sender, "Burn: caller is not the owner");
+    function burn(address account, uint256 id, uint256 amount) public {
+        require(
+            balanceOf(account, id) >= amount,
+            "Burn: caller insufficient amount"
+        );
         _burn(account, id, amount);
         _totalSupply[id] -= amount;
     }
@@ -76,10 +160,10 @@ contract Collection1155 is ERC1155, Ownable {
         address account,
         uint256[] memory ids,
         uint256[] memory amounts
-    ) public onlyOwner {
-        require(account == msg.sender, "Burn: caller is not the owner");
-        _burnBatch(account, ids, amounts);
+    ) public {
+        require(ids.length == amounts.length);
         for (uint256 i = 0; i < ids.length; i++) {
+            burn(account, ids[i], amounts[i]);
             _totalSupply[ids[i]] -= amounts[i];
         }
     }
--- contracts/Collection721.sol
+++ contracts/Collection721.sol
@@ -7,10 +7,19 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "./IGmpReceiver.sol";
+import "./IGateway.sol";
 
-contract Collection721 is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract Collection721 is
+    ERC721,
+    ERC721URIStorage,
+    ERC721Burnable,
+    Ownable,
+    IGmpReceiver
+{
     using Strings for uint256;
 
+    address public immutable _gateway;
     uint256 private _nextTokenId;
     uint16 private _nextDropId;
     string public baseURI;
@@ -43,14 +52,23 @@ contract Collection721 is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         uint256 whiteListPrice;
     }
 
+    struct CrosschainMessage {
+        uint256 tokenId;
+        address newHolder;
+    }
+
     mapping(uint16 => Drop) private DropList;
+    mapping(uint256 => address) private _burntBy;
+    mapping(uint16 => address) private _crosschainContract;
 
     constructor(
         string memory _name,
         string memory _symbol,
-        string memory _logoURI
+        string memory _logoURI,
+        address gateway
     ) ERC721(_name, _symbol) Ownable(msg.sender) {
         logoURI = _logoURI;
+        _gateway = gateway;
     }
 
     modifier onlyDuringDrop() {
@@ -88,6 +106,63 @@ contract Collection721 is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         _;
     }
 
+    function setCrosschainAddress(
+        uint16 _network,
+        address _contractAddress
+    ) public onlyOwner {
+        _crosschainContract[_network] = _contractAddress;
+    }
+
+    // asuming the gateway already handles verification
+    // we just need to mint on received
+    function onGmpReceived(
+        bytes32,
+        uint128,
+        bytes32,
+        bytes calldata b
+    ) external payable returns (bytes32) {
+        require(msg.sender == _gateway, "unauthorized");
+        CrosschainMessage memory message = abi.decode(b, (CrosschainMessage));
+        _safeMint(message.newHolder, message.tokenId);
+        _burntBy[message.tokenId] = address(0);
+
+        return bytes32("");
+    }
+
+    function encodeCrosschainMessage(
+        uint256 _tokenId,
+        address _newHolder
+    ) public pure returns (bytes memory) {
+        CrosschainMessage memory message = CrosschainMessage(
+            _tokenId,
+            _newHolder
+        );
+        return abi.encode(message);
+    }
+
+    function crosschainTransfer(
+        uint256 _tokenId,
+        address _newHolder,
+        uint16 _destinationNetwork
+    ) public {
+        require(
+            _crosschainContract[_destinationNetwork] != address(0),
+            "Crosschain: Unsupport Network"
+        );
+        require(
+            _ownerOf(_tokenId) == msg.sender,
+            "Crosschain: Token is not owned by Sender"
+        );
+        burn(_tokenId);
+        IGateway gateway = IGateway(_gateway);
+        gateway.submitMessage(
+            _crosschainContract[_destinationNetwork],
+            _destinationNetwork,
+            3000000,
+            encodeCrosschainMessage(_tokenId, _newHolder)
+        );
+    }
+
     function currentDrop() public view returns (DropReturn memory) {
         Drop storage drop = DropList[_nextDropId];
         DropReturn memory dropReturn = DropReturn(
@@ -102,6 +177,15 @@ contract Collection721 is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
             drop.whiteListPrice
         );
         return dropReturn;
+    }
+
+    function burn(uint256 _tokenId) public override {
+        super.burn(_tokenId);
+        _burntBy[_tokenId] = msg.sender;
+    }
+
+    function burntBy(uint256 _tokenId) public view returns (address) {
+        return _burntBy[_tokenId];
     }
 
     function getWhiteListAccess(
